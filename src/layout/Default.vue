301ca2c0dd9581372771fcff20806ba83e005458
zky1994 <229763015@qq.com>
2020-09-27 19:23:51 +0800

    初始化大布局

--- src/layout/Default.vue
+++ src/layout/Default.vue
@@ -5,10 +5,19 @@
       <User />
     </a-layout-header>
     <a-layout>
-<!--      <a-layout-sider>Sider</a-layout-sider>-->
-      <a-layout-content>
-        <router-view />
-      </a-layout-content>
+      <a-layout-sider style="background: #fff">Sider</a-layout-sider>
+      <a-layout style="padding: 0 24px 24px">
+        <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item>List</a-breadcrumb-item>
+          <a-breadcrumb-item>App</a-breadcrumb-item>
+        </a-breadcrumb>
+        <a-layout-content
+            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+          <router-view />
+        </a-layout-content>
+      </a-layout>
     </a-layout>
     <a-drawer
         title="系统配置"
